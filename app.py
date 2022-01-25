11d100110e3f74eb4b8cf1c9c2e18bba55a706e6
marcosvile <marcosvilelateixeira@gmail.com>
2022-01-24 23:35:47 -0300

    commit

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/about.html")
+def explorar():
+    return render_template('about.html')
+
+@app.route("/contact.html")
+def contato():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
