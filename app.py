3f6782ebb9d6ed7a777c54477a2e2ee6014ba0bf
marcosvile <marcosvilelateixeira@gmail.com>
2022-01-31 11:23:34 -0300

    adequando as rotas

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/index.html")
+def index2():
+    return render_template('index.html')
+
 @app.route("/about.html")
 def explorar():
     return render_template('about.html')
